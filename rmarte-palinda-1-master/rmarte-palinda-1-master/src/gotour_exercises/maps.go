56a72a5c1c74b3e6da47f9fbb63cfc1032f4afd1
leopoldh <leopold.hermansson@gmail.com>
2021-03-23 14:41:17 +0100

    done

---
+++ rmarte-palinda-1-master/rmarte-palinda-1-master/src/gotour_exercises/maps.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	a := strings.Fields(s)
+	b := map[string]int{}
+	for i:=0;i < len(a); i++{
+		b[a[i]]++
+	}
+	return b
+}
+
+func main() {
+	wc.Test(WordCount)
+}
---
+++ rmarte-palinda-1-master/rmarte-palinda-1-master/src/gotour_exercises/slices.go
@@ -0,0 +1,19 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	a:= make([][]uint8, dy)       
+	for i:=0;i<dy;i++ {
+		a[i] = make([]uint8, dx) 
+	}
+	for y := 0; y < dy; y++ {
+		for x := 0; x < dx;x++ {
+			a[y][x] = uint8(x*y)
+		}
+	}
+	return a
+}
+func main() {
+	pic.Show(Pic)
+}
